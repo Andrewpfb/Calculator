c46166992b61e1c691aea4782030f76fa14594cc
Andrew Golik <AndrewGolik29@gmail.com>
2017-09-16 14:32:33 +0300

    Release 1.1 Ready: 1)Basic functional; 2)Pi,E Not ready: 1). functional (convert int to double)

--- app/src/main/java/by/belstu/fit/golik/calculator/Operation.java
+++ app/src/main/java/by/belstu/fit/golik/calculator/Operation.java
@@ -41,47 +41,36 @@ public class Operation
     static public int One() {
         return 1;
     }
-
     static public int Two() {
         return 2;
     }
-
     static public int Three() {
         return 3;
     }
-
     static public int Four() {
         return 4;
     }
-
     static public int Five() {
         return 5;
     }
-
     static public int Six() {
         return 6;
     }
-
     static public int Seven() {
         return 7;
     }
-
     static public int Eight() {
         return 8;
     }
-
     static public int Nine() {
         return 9;
     }
-
     static public int Zero() {
         return 0;
     }
-
     static public double Pi() {
         return Math.PI;
     }
-
     static public double E() {
         return Math.E;
     }
